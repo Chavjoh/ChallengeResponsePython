552a5a0a98d61a1d7568129c1ed1f65d48ad6b2f
Chavjoh <j.chavaillaz@gmail.com>
2014-04-08 11:14:28 +0200

    First version of the server script.

---
+++ Server.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------------#
+# Security - Challenge Response Client / Server                                #
+# ============================================================================ #
+# Developer:    Chavaillaz Johan                                               #
+# Filename:     ChallengeResponseServer.py                                     #
+# Version:      1.0                                                            #
+#                                                                              #
+# Licensed to the Apache Software Foundation (ASF) under one                   #
+# or more contributor license agreements. See the NOTICE file                  #
+# distributed with this work for additional information                        #
+# regarding copyright ownership. The ASF licenses this file                    #
+# to you under the Apache License, Version 2.0 (the                            #
+# "License"); you may not use this file except in compliance                   #
+# with the License. You may obtain a copy of the License at                    #
+#                                                                              #
+# http://www.apache.org/licenses/LICENSE-2.0                                   #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing,                   #
+# software distributed under the License is distributed on an                  #
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY                       #
+# KIND, either express or implied. See the License for the                     #
+# specific language governing permissions and limitations                      #
+# under the License.                                                           #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#                               LIBRARIES IMPORT                               #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+import socket
+import sys
+import threading
+import argparse
+import re
+import uuid
+
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64, os
+
+import hashlib
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#                             UTILITIES FUNCTIONS                              #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+def prepareString(content):
+	return bytes(content + "\n", 'UTF-8')
+
+def getArguments(message):
+	return message.split(" ")[1:]
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#                              RESPONSE FUNCTIONS                              #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+def helloRequest(thread):
+	thread.connection.send(prepareString("MSG Hello from the server !"))
+
+def loginRequest(thread):
+	user = getArguments(thread.message)[0]
+	#userManager = UserManager.Instance()
+	#if user in userManager.users.keys():
+	thread.data['token'] = str(uuid.uuid4())
+	thread.connection.send(prepareString("CHALLENGE " + thread.data['token']))
+	
+def challengeRequest(thread):
+	passwordMessage = thread.data['token'] + "password" # TODO load PWD
+	hashObject = hashlib.sha256(passwordMessage.encode('UTF-8'))
+	hexDigest = hashObject.hexdigest() 
+	
+	# Get hash from received message
+	hashRequest = getArguments(thread.message)[0]
+	
+	# Test connection
+	if hexDigest == hashRequest:
+		thread.data['login'] = True
+		thread.connection.send(prepareString("LOGIN SUCCESS"))
+	else:
+		thread.data['token'] = ""
+		thread.connection.send(prepareString("LOGIN FAILURE"))
+	
+def isLoggedRequest(thread):
+	if 'login' in thread.data.keys():
+		thread.connection.send(prepareString("MSG You are logged in."))
+	else:
+		thread.connection.send(prepareString("MSG You are not logged in."))
+
+def logoutRequest(thread):
+	if 'login' in thread.data.keys():
+		del thread.data['login']
+		thread.connection.send(prepareString("MSG You are now logged out."))
+	else:
+		thread.connection.send(prepareString("MSG You are not logged in."))
+	
+def closeConnectionRequest(thread):
+	thread.connection.send(prepareString("CLOSE CONFIRM"))
+	thread.closeConnection = True
+
+def notFoundRequest(thread):
+	thread.connection.send(prepareString("MSG Command not found"))
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#                                   CLASSES                                    #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+class ThreadClient(threading.Thread):
+	'''Manage each client connection to the server in a new thread'''
+	
+	functionArray = {
+		r"HELLO": helloRequest,
+		r"LOGIN .*": loginRequest, 
+		r"CHALLENGE .*": challengeRequest, 
+		r"IS_LOGGED": isLoggedRequest,
+		r"LOGOUT": logoutRequest,
+		r"CLOSE": closeConnectionRequest,
+		r".*": notFoundRequest,
+	} 
+	
+	def __init__(self, connection):
+		threading.Thread.__init__(self)
+		self.connection = connection
+		self.closeConnection = False
+		self.data = {}
+		self.message = ""
+
+	def run(self):
+		threadName = self.getName()
+
+		try:
+			while not self.closeConnection:
+				character = self.connection.recv(1)
+				
+				if character == b'\n':
+				
+					for regex, function in ThreadClient.functionArray.items():
+						if re.match(regex, self.message):
+							function(self)
+							break
+					
+					print("%s> %s" % (threadName, self.message))
+					
+					self.message = ""
+				else:
+					self.message += character.decode("utf-8")
+		
+		except ConnectionResetError:
+			print("Connection reset by client.")
+		
+		# Close connection
+		self.connection.close()
+		del connectionList[threadName]
+		print("Client %s disconnected." % threadName)
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#                               "MAIN" FUNCTION                                #
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+# If this is the main module, run this
+if __name__ == '__main__':
+	argsCount = len(sys.argv)
+
+	# Create argument parser to help user
+	parser = argparse.ArgumentParser(
+		description='Server instance for challenge response application.'
+	)
+	parser.add_argument(
+		'database', 
+		type=str,
+		help='Database of users and password to use for challenge response.'
+	)
+	parser.add_argument(
+		'port', 
+		nargs='+',
+		type=str,
+		help='Port to used for the server.'
+	)
+
+	# Show help if one of the arguments is missing
+	if argsCount not in [2, 3]:
+		parser.print_help()
+		sys.exit()
+	
+	# Get configuration
+	host = '127.0.0.1'
+	database = sys.argv[1]
+	port = int(sys.argv[2]) if argsCount == 3 else 1991
+	
+	# Server initialization
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		serverSocket.bind((host, port))
+	except socket.error:
+		print("The connection socket to the selected address has failed.")
+		sys.exit()
+	
+	# Listen new connection
+	print("Server ready, waiting for requests ...")
+	serverSocket.listen(1)
+
+	# Connection list (clients)
+	connectionList = {}
+	
+	try:
+		while True:    
+			connection, adresse = serverSocket.accept()
+			
+			# Start a new client
+			thread = ThreadClient(connection)
+			thread.start()
+			
+			# Store connection
+			id = thread.getName()
+			connectionList[id] = connection
+			
+			# Send a welcome message
+			connection.send(prepareString("MSG Welcome aboard !"))
+	
+	except KeyboardInterrupt:
+		print("Server shut down")
